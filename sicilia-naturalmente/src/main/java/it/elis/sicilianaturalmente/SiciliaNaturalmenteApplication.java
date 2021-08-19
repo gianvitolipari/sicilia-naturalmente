4f088c385766238c1c27cbfda896a194279ea972
gianvitolipari <gianvito.lipari.elis@gmail.com>
2021-08-19 14:56:09 +0200

    upload

--- sicilia-naturalmente/src/main/java/it/elis/sicilianaturalmente/SiciliaNaturalmenteApplication.java
+++ sicilia-naturalmente/src/main/java/it/elis/sicilianaturalmente/SiciliaNaturalmenteApplication.java
@@ -2,7 +2,10 @@ package it.elis.sicilianaturalmente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.persistence.EntityManager;
 
@@ -11,5 +14,15 @@ public class SiciliaNaturalmenteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SiciliaNaturalmenteApplication.class, args);
 
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://172.23.0.1:3000");
+			}
+		};
 	}
 }
